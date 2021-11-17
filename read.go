4ecbd403c15b00372694050a459f1b2c07fc0964
Rogee <rogeecn@gmail.com>
2021-11-17 15:43:49 +0800

    add read byte

--- read.go
+++ read.go
@@ -17,6 +17,16 @@ func New(h wingo.HPROCESS) HProcess {
 	return HProcess{h}
 }
 
+// Read process memory and convert the returned data to byte
+func (h HProcess) ReadByte(lpBaseAddress uint32) (byte, error) {
+	data, err := h.hProcess.ReadProcessMemory(lpBaseAddress, 4)
+	if err != nil {
+		return 0, err
+	}
+
+	return data[0], nil
+}
+
 // Read process memory and convert the returned data to uint32
 func (h HProcess) ReadUint32(lpBaseAddress uint32) (uint32, error) {
 	data, err := h.hProcess.ReadProcessMemory(lpBaseAddress, 4)
